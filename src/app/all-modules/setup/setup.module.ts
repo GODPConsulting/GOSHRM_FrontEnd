e7301706b5573cd3981b4dc8748e04e6f7f7bbad
BrunoElo <elochux@gmail.com>
2020-12-04 16:24:38 +0100

    feat: implemented employment type component

--- src/app/all-modules/setup/setup.module.ts
+++ src/app/all-modules/setup/setup.module.ts
@@ -10,13 +10,15 @@ import { DataTablesModule } from "angular-datatables";
 import {ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {HttpTokenInterceptor} from "../../services/http.interceptor.service";
+import { EmploymentTypeComponent } from './employment-type/employment-type.component';
 
 @NgModule({
   declarations: [
     SetupComponent,
     JobgradesComponent,
     HighSchoolSubjectComponent,
-    HighSchoolSubjectsComponent
+    HighSchoolSubjectsComponent,
+    EmploymentTypeComponent
   ],
   imports: [CommonModule, SetupRoutingModule, DataTablesModule, ReactiveFormsModule],
   providers: [
--- src/app/services/setup.service.ts
+++ src/app/services/setup.service.ts
@@ -1,29 +1,61 @@
-import { Injectable } from '@angular/core';
-import {ApiService} from "./api.service";
-import {Observable} from "rxjs";
-import {tap} from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { ApiService } from "./api.service";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SetupService {
-
-  constructor(
-    private apiService: ApiService
-  ) { }
+  constructor(private apiService: ApiService) {}
   getHighSchoolSubject(): Observable<any> {
-    return this.apiService.get(`/hrmsetup/get/all/highschoolsubjects`).pipe(tap(data => {
-      return data;
-    }))
+    return this.apiService.get(`/hrmsetup/get/all/highschoolsubjects`).pipe(
+      tap((data) => {
+        return data;
+      })
+    );
   }
   updateHighSchoolSubject(payload): Observable<any> {
-    return this.apiService.post(`/hrmsetup/add/update/highschoolsubject`, payload).pipe(res => {
-      return res;
-    })
+    return this.apiService
+      .post(`/hrmsetup/add/update/highschoolsubject`, payload)
+      .pipe((res) => {
+        return res;
+      });
   }
-  deleteHighSchoolSubject(payload):Observable<any> {
-    return this.apiService.post(`/hrmsetup/delete/highschoolsubject`, payload).pipe(tap(data => {
-      return data;
-    }))
+  deleteHighSchoolSubject(payload): Observable<any> {
+    return this.apiService
+      .post(`/hrmsetup/delete/highschoolsubject`, payload)
+      .pipe(
+        tap((data) => {
+          return data;
+        })
+      );
+  }
+
+  // Employment Type
+  getEmploymentTypeApi(): Observable<any> {
+    return this.apiService.get(`/hrmsetup/get/all/employmenttypes`).pipe(
+      tap((data) => {
+        return data;
+      })
+    );
+  }
+
+  updateEmploymentType(payload): Observable<any> {
+    return this.apiService
+      .post(`/hrmsetup/add/update/employmenttype`, payload)
+      .pipe((res) => {
+        return res;
+      });
+  }
+
+  deleteEmploymentType(payload): Observable<any> {
+    return this.apiService
+      .post(`/hrmsetup/delete/employmenttype`, payload)
+      .pipe(
+        tap((data) => {
+          return data;
+        })
+      );
   }
 }
