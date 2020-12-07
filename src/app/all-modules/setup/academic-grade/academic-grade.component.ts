1624af486069b962b95e2ff519c108c21f317df4
alfredadeshola <alfredadeshola@gmail.com>
2020-12-07 08:59:25 +0100

    Added Academic Grade

---
+++ src/app/all-modules/setup/academic-grade/academic-grade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-academic-grade',
+  templateUrl: './academic-grade.component.html',
+  styleUrls: ['./academic-grade.component.css']
+})
+export class AcademicGradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/all-modules/setup/setup.module.ts
+++ src/app/all-modules/setup/setup.module.ts
@@ -10,13 +10,15 @@ import { DataTablesModule } from "angular-datatables";
 import {ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {HttpTokenInterceptor} from "../../services/http.interceptor.service";
+import { AcademicGradeComponent } from './academic-grade/academic-grade.component';
 
 @NgModule({
   declarations: [
     SetupComponent,
     JobgradesComponent,
     HighSchoolSubjectComponent,
-    HighSchoolSubjectsComponent
+    HighSchoolSubjectsComponent,
+    AcademicGradeComponent
   ],
   imports: [CommonModule, SetupRoutingModule, DataTablesModule, ReactiveFormsModule],
   providers: [
