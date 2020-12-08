458172e65e460c5ca7918483b3fa30242aae7cea
alfredadeshola <alfredadeshola@gmail.com>
2020-12-08 14:13:23 +0100

    Added new component resolved rebase conflict

---
+++ src/app/all-modules/setup/prof-certification/prof-certification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prof-certification',
+  templateUrl: './prof-certification.component.html',
+  styleUrls: ['./prof-certification.component.css']
+})
+export class ProfCertificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
