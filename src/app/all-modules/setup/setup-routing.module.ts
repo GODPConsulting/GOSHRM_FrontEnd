733e48e61be3577ac41f85ac61db54386339ad4e
BrunoElo <elochux@gmail.com>
2020-12-08 15:01:54 +0100

    rectified minor issues

--- src/app/all-modules/setup/setup-routing.module.ts
+++ src/app/all-modules/setup/setup-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { SetupComponent } from "./setup.component";
-import { HighSchoolSubjectComponent } from "./high-school-subject/high-school-subject.component";
 import { HighSchoolSubjectsComponent } from "./high-school-subjects/high-school-subjects.component";
 import { EmploymentTypeComponent } from "./employment-type/employment-type.component";
 import { JobGradeComponent } from "./job-grade/job-grade.component";
 import { AcademicDisciplineComponent } from "./academic-discipline/academic-discipline.component";
 import { EmploymentLevelComponent } from "./employment-level/employment-level.component";
-import { AcademicQualificationComponent } from './academic-qualification/academic-qualification.component';
+import { AcademicQualificationComponent } from "./academic-qualification/academic-qualification.component";
 import { HmoComponent } from "./hmo/hmo.component";
-import { AcademicGradeComponent } from './academic-grade/academic-grade.component';
+import { AcademicGradeComponent } from "./academic-grade/academic-grade.component";
 
 const routes: Routes = [
   {
@@ -29,16 +28,16 @@ const routes: Routes = [
         component: HighSchoolSubjectsComponent,
       },
       {
-        path: 'employment-level',
-        component: EmploymentLevelComponent
+        path: "employment-level",
+        component: EmploymentLevelComponent,
       },
       {
-        path: 'academic-discipline',
-        component: AcademicDisciplineComponent
+        path: "academic-discipline",
+        component: AcademicDisciplineComponent,
       },
       {
-        path: 'academic-qualification',
-        component: AcademicQualificationComponent
+        path: "academic-qualification",
+        component: AcademicQualificationComponent,
       },
       {
         path: "hmo",
