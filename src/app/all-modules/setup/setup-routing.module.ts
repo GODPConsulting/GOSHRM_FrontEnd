60c9b1c18dc1d38e24a9447f42828d19d50d1641
ojewaletope <topeapps@gmail.com>
2020-12-14 09:33:50 +0100

    worked on set up routing module and sidebar component

--- src/app/all-modules/setup/setup-routing.module.ts
+++ src/app/all-modules/setup/setup-routing.module.ts
@@ -3,14 +3,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { SetupComponent } from "./setup.component";
 import { HighSchoolSubjectsComponent } from "./high-school-subjects/high-school-subjects.component";
 import { EmploymentTypeComponent } from "./employment-type/employment-type.component";
-import { JobGradeComponent } from "./job-grade/job-grade.component";
 import { AcademicDisciplineComponent } from "./academic-discipline/academic-discipline.component";
 import { EmploymentLevelComponent } from "./employment-level/employment-level.component";
-import { AcademicQualificationComponent } from "./academic-qualification/academic-qualification.component";
-import { HmoComponent } from "./hmo/hmo.component";
-import { GymWorkoutComponent } from "./gym-workout/gym-workout.component";
-import { HighSchoolGradeComponent } from "./high-school-grade/high-school-grade.component";
-import { AcademicGradeComponent } from "./academic-grade/academic-grade.component";
+import {AcademicQualificationComponent} from "./academic-qualification/academic-qualification.component";
+import {JobGradeComponent} from "./job-grade/job-grade.component";
+import {HmoComponent} from "./hmo/hmo.component";
+import {GymWorkoutComponent} from "./gym-workout/gym-workout.component";
+import {HighSchoolGradeComponent} from "./high-school-grade/high-school-grade.component";
+import {AcademicGradeComponent} from "./academic-grade/academic-grade.component";
+
 
 const routes: Routes = [
   {
