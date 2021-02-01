63418d12e53abd402dae3fba4095d910475400c0
BrunoElo <elochux@gmail.com>
2021-02-02 06:51:46 +1300

    integrated the crop functionality into the employee form

--- src/app/all-modules/employees/employees.module.ts
+++ src/app/all-modules/employees/employees.module.ts
@@ -16,19 +16,20 @@ import { HttpTokenInterceptor } from "../../services/http.interceptor.service";
 import { EmployeeFormComponent } from "./all-employees/employee-form/employee-form.component";
 import { SharedModule } from "src/app/shared/shared.module";
 import { NgSelectModule } from "@ng-select/ng-select";
-import { IdentificationComponent } from './all-employees/employee-profile/identification/identification.component';
-import { HobbiesComponent } from './all-employees/employee-profile/hobbies/hobbies.component';
-import { AssetsComponent } from './all-employees/employee-profile/assets/assets.component';
-import { RefereeComponent } from './all-employees/employee-profile/referee/referee.component';
-import { ProfCertComponent } from './all-employees/employee-profile/prof-cert/prof-cert.component';
-import { DependentContactComponent } from './all-employees/employee-profile/dependent-contact/dependent-contact.component';
-import { CareerComponent } from './all-employees/employee-profile/career/career.component';
+import { IdentificationComponent } from "./all-employees/employee-profile/identification/identification.component";
+import { HobbiesComponent } from "./all-employees/employee-profile/hobbies/hobbies.component";
+import { AssetsComponent } from "./all-employees/employee-profile/assets/assets.component";
+import { RefereeComponent } from "./all-employees/employee-profile/referee/referee.component";
+import { ProfCertComponent } from "./all-employees/employee-profile/prof-cert/prof-cert.component";
+import { DependentContactComponent } from "./all-employees/employee-profile/dependent-contact/dependent-contact.component";
+import { CareerComponent } from "./all-employees/employee-profile/career/career.component";
 
 import { SkillsComponent } from "./all-employees/employee-profile/skills/skills.component";
 import { EmployeeHmoComponent } from "./all-employees/employee-profile/employee-hmo/employee-hmo.component";
 import { RatingModule } from "ngx-bootstrap/rating";
 import { HospitalComponent } from "./all-employees/employee-profile/hospital/hospital.component";
 import { EmployeeGymComponent } from "./all-employees/employee-profile/employee-gym/employee-gym.component";
+
 @NgModule({
   declarations: [
     EmployeesComponent,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {LoadingComponent} from "./loading/loading.component";
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import {AppMenuComponent, AppSubMenuComponent} from "./app.menu.component";
-import {RouterModule} from "@angular/router";
-import { NgSelectModule } from '@ng-select/ng-select';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoadingComponent } from "./loading/loading.component";
+import { MainLayoutComponent } from "./main-layout/main-layout.component";
+import { AppMenuComponent, AppSubMenuComponent } from "./app.menu.component";
+import { RouterModule } from "@angular/router";
+import { NgSelectModule } from "@ng-select/ng-select";
+import { ImageCropperModule } from "ngx-image-cropper";
 
 @NgModule({
   declarations: [LoadingComponent, AppMenuComponent, AppSubMenuComponent],
   imports: [
     CommonModule,
     RouterModule,
+    ImageCropperModule,
     // NgSelectModule
   ],
-  exports: [LoadingComponent, AppMenuComponent, AppSubMenuComponent]
+  exports: [
+    LoadingComponent,
+    AppMenuComponent,
+    AppSubMenuComponent,
+    ImageCropperModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
