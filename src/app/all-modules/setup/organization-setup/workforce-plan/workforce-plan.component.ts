7a484440528403b5f76d6c139cc65bbf599e90ea
Vicez <akinseindevictor@gmail.com>
2022-05-27 11:18:25 +0200

    update

--- src/app/all-modules/setup/organization-setup/workforce-plan/workforce-plan.component.ts
+++ src/app/all-modules/setup/organization-setup/workforce-plan/workforce-plan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkforcePlanModel } from '../models/workforce-plan.model';
 
 @Component({
   selector: 'app-workforce-plan',
@@ -6,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./workforce-plan.component.css']
 })
 export class WorkforcePlanComponent implements OnInit {
+
+  workfocePlanData: WorkforcePlanModel[] = [
+    {
+      dateCreated: '21 Sep, 2020',
+      office: 'APOLLO Architects & Associates',
+      budget: '$ 6000.00',
+      completionStatus: 'Ongoing',
+      approvalStatus: 'pending'
+    },
+    {
+      dateCreated: '22 Oct, 2020',
+      office: 'VTN Architects',
+      budget: '$ 526.90',
+      completionStatus: 'Ongoing',
+      approvalStatus: 'pending'
+    },
+    {
+      dateCreated: '24 May, 2020',
+      office: 'M-Arquitectos',
+      budget: '$ 57,5944.00',
+      completionStatus: 'Completed',
+      approvalStatus: 'pending'
+    }
+  ]
 
   constructor() { }
 
