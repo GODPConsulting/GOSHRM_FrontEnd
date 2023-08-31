41103d8c4f575e7f11dddb7bd3f22122dc8fe484
Joshua T Chukwu <57834390+JoshuaTChukwu@users.noreply.github.com>
2023-08-31 20:51:58 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // api_url: "https://api.goserp.co.uk/learning",
-  api_url: "http://92.205.27.45:97/api/v1/learning",
+  api_url: "https://lmsback.goserp.co.uk/api/v1/learning",
   gateway_url: "https://api.goserp.co.uk",
 };
